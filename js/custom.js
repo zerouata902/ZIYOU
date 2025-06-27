a9163a8beae0def2f002edee19a6c9ccacd39f56
saeed_811 <yzerwata@gmail.com>
2025-06-28 00:25:39 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -30,40 +30,40 @@
     
     })(window.jQuery);
 
-    document.getElementById("bookingForm").addEventListener("submit", function (e) {
-      e.preventDefault();
-    
-      const fname = document.getElementById("fname").value;
-      const lname = document.getElementById("lname").value;
-      const age = document.getElementById("age").value;
-      const birthdate = document.getElementById("birthdate").value;
-      const level = document.getElementById("level").value;
-      const location = document.getElementById("location").value;
-    
-      const message = `🧔‍♂️ طلب تسجيل في Ziyou Barber Academy:\n
-    الاسم: ${fname}
-    النسب: ${lname}
-    العمر: ${age}
-    تاريخ الازدياد: ${birthdate}
-    المستوى الدراسي: ${level}
-    مكان السكن: ${location}`;
-    
-      const whatsappNumber = "+2126826204"; // ← بدل برقم الواتساب ديال Ziyou
-      const encodedMsg = encodeURIComponent(message);
-      const whatsappURL = https://wa.me/${whatsappNumber}?text=${encodedMsg};
-    
-      window.open(whatsappURL, "_blank");
-    });
-    
-    // scroll effect
-    window.addEventListener("scroll", function () {
-      const box = document.querySelector(".form-box");
-      const rect = box.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
-    
-      if (rect.top < windowHeight - 100) {
-        box.classList.add("visible");
-      }
-    });
+document.getElementById("bookingForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  const age = document.getElementById("age").value;
+  const birthdate = document.getElementById("birthdate").value;
+  const level = document.getElementById("level").value;
+  const location = document.getElementById("location").value;
+
+  const message = `🧔‍♂️ طلب تسجيل في Ziyou Barber Academy:\n
+الاسم: ${fname}
+النسب: ${lname}
+العمر: ${age}
+تاريخ الازدياد: ${birthdate}
+المستوى الدراسي: ${level}
+مكان السكن: ${location}`;
+
+  const whatsappNumber = "+212676826204"; // ← بدل برقم الواتساب ديال Ziyou
+  const encodedMsg = encodeURIComponent(message);
+  const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodedMsg}`;
+
+  window.open(whatsappURL, "_blank");
+});
+
+// scroll effect
+window.addEventListener("scroll", function () {
+  const box = document.querySelector(".form-box");
+  const rect = box.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  if (rect.top < windowHeight - 100) {
+    box.classList.add("visible");
+  }
+});
 
 
